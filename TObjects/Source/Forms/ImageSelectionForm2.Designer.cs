3b3541cb97529d0aeb28d5fa5868737a13e85bc6
Markus Lobedann <markus.lobedann@gmail.com>
2017-06-09 13:43:01 +0200

    - created an alternative ImageSelectionForm (which is not uses anymore) - upgrades JSON.Net to version 10.0.2 - more TODOs

---
+++ TObjects/Source/Forms/ImageSelectionForm2.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Tesserakt
+{
+    partial class ImageSelectionForm2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if( disposing && ( components != null ) )
+            {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxImage = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxImage
+            // 
+            this.pictureBoxImage.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.pictureBoxImage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxImage.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxImage.Name = "pictureBoxImage";
+            this.pictureBoxImage.Size = new System.Drawing.Size(286, 498);
+            this.pictureBoxImage.TabIndex = 0;
+            this.pictureBoxImage.TabStop = false;
+            this.pictureBoxImage.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBoxImage_Paint);
+            this.pictureBoxImage.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBoxImage_MouseDown);
+            this.pictureBoxImage.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBoxImage_MouseMove);
+            this.pictureBoxImage.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBoxImage_MouseUp);
+            // 
+            // ImageSelectionForm2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(286, 498);
+            this.Controls.Add(this.pictureBoxImage);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.KeyPreview = true;
+            this.MinimumSize = new System.Drawing.Size(150, 150);
+            this.Name = "ImageSelectionForm2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ImageSelectionForm_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxImage;
+    }
+}
---
+++ TObjects/Source/Forms/ImageSelectionForm2.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace Tesserakt
+{
+    public partial class ImageSelectionForm2 : Form
+    {
+        public ImageSelectionForm2( string title, Image img, Size maxImageSize )
+        {
+            InitializeComponent();
+
+            this.Text = title;
+
+            m_maxImageSize = maxImageSize;
+
+            m_img = img;
+
+            movingPoint.X = Convert.ToInt32( ( pictureBoxImage.Width / 2 ) - ( m_img.Width / 2 ) );
+            movingPoint.Y = Convert.ToInt32( ( pictureBoxImage.Height / 2 ) - ( m_img.Height / 2 ) );
+
+            transform.Translate( movingPoint.X, movingPoint.Y );
+
+            pictureBoxImage.Select();
+
+            ClientSize = new Size( 100, 200 );
+
+            pictureBoxImage.MouseWheel += PictureBoxImage_MouseWheel;
+        }
+
+        private void ImageSelectionForm_KeyDown( object sender, KeyEventArgs e )
+        {
+            if( e.KeyCode == Keys.Escape )
+            {
+                this.Close();
+            }
+        }
+
+        public Image Image
+        {
+            get;
+            private set;
+        }
+
+        Matrix transform = new Matrix();
+
+        private const float s_dScaleValue = 0.1f;
+
+        private readonly Image m_img;
+
+        private readonly Size m_maxImageSize;
+
+        private const int baseSize = 80;
+
+        private Point startingPoint = Point.Empty;
+        private Point movingPoint = Point.Empty;
+        private bool panning = false;
+
+        private void pictureBoxImage_Paint( object sender, PaintEventArgs e )
+        {
+            e.Graphics.Clear( Color.White );
+
+            e.Graphics.Transform = transform;
+
+            e.Graphics.DrawImage( m_img, Point.Empty );
+        }
+
+        private void pictureBoxImage_MouseDown( object sender, MouseEventArgs e )
+        {
+            pictureBoxImage.Cursor = Cursors.NoMove2D;
+
+            panning = true;
+
+            startingPoint = e.Location;
+        }
+
+        private void pictureBoxImage_MouseUp( object sender, MouseEventArgs e )
+        {
+            pictureBoxImage.Cursor = Cursors.SizeAll;
+
+            panning = false;
+        }
+
+        private void pictureBoxImage_MouseMove( object sender, MouseEventArgs e )
+        {
+            if( panning )
+            {
+                movingPoint = new Point( e.Location.X - startingPoint.X, e.Location.Y - startingPoint.Y );
+
+                startingPoint = e.Location;
+
+                transform.Translate( movingPoint.X, movingPoint.Y, MatrixOrder.Append );
+
+                pictureBoxImage.Invalidate();
+            }
+        }
+
+        private void PictureBoxImage_MouseWheel( object sender, MouseEventArgs e )
+        {
+            if( pictureBoxImage.Focused && e.Delta != 0 )
+            {
+                transform.Translate( -e.Location.X, -e.Location.Y, MatrixOrder.Append );
+
+                if( e.Delta > 0 )
+                {
+                    transform.Scale( 1 + s_dScaleValue, 1 + s_dScaleValue, MatrixOrder.Append );
+                }
+                else
+                {
+                    transform.Scale( 1 - s_dScaleValue, 1 - s_dScaleValue, MatrixOrder.Append );
+                }
+
+                transform.Translate( e.Location.X, e.Location.Y, MatrixOrder.Append );
+
+                pictureBoxImage.Invalidate();
+            }
+        }
+    }
+}
