2bc765039dfedfc039154c154f8905ddb654cdfb
Markus Lobedann <markus.lobedann@gmail.com>
2017-06-14 16:41:03 +0200

    nicer buttons in the main form for the MasterData

--- MasterData/source/MasterDataMainForm.Designer.cs
+++ MasterData/source/MasterDataMainForm.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MasterDataMainForm));
+            this.components = new System.ComponentModel.Container();
             this.buttonWeapons = new System.Windows.Forms.Button();
             this.buttonArmor = new System.Windows.Forms.Button();
             this.buttonQuit = new System.Windows.Forms.Button();
@@ -36,44 +36,47 @@
             this.buttonEquipment = new System.Windows.Forms.Button();
             this.buttonFactions = new System.Windows.Forms.Button();
             this.buttonActors = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // buttonWeapons
             // 
-            this.buttonWeapons.Dock = System.Windows.Forms.DockStyle.Top;
-            this.buttonWeapons.Image = ((System.Drawing.Image)(resources.GetObject("buttonWeapons.Image")));
-            this.buttonWeapons.Location = new System.Drawing.Point(0, 34);
+            this.buttonWeapons.AutoSize = true;
+            this.buttonWeapons.BackgroundImage = global::Tesserakt.Properties.Resources.weapons;
+            this.buttonWeapons.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonWeapons.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonWeapons.Location = new System.Drawing.Point(164, 3);
             this.buttonWeapons.Name = "buttonWeapons";
-            this.buttonWeapons.Size = new System.Drawing.Size(234, 34);
+            this.buttonWeapons.Size = new System.Drawing.Size(155, 108);
             this.buttonWeapons.TabIndex = 1;
-            this.buttonWeapons.Text = "&Waffen";
-            this.buttonWeapons.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.buttonWeapons.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.toolTip1.SetToolTip(this.buttonWeapons, "Waffen");
             this.buttonWeapons.UseVisualStyleBackColor = true;
             this.buttonWeapons.Click += new System.EventHandler(this.buttonWeapons_Click);
             // 
             // buttonArmor
             // 
-            this.buttonArmor.Dock = System.Windows.Forms.DockStyle.Top;
-            this.buttonArmor.Image = ((System.Drawing.Image)(resources.GetObject("buttonArmor.Image")));
-            this.buttonArmor.Location = new System.Drawing.Point(0, 68);
+            this.buttonArmor.AutoSize = true;
+            this.buttonArmor.BackgroundImage = global::Tesserakt.Properties.Resources.armor;
+            this.buttonArmor.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonArmor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonArmor.Location = new System.Drawing.Point(325, 3);
             this.buttonArmor.Name = "buttonArmor";
-            this.buttonArmor.Size = new System.Drawing.Size(234, 34);
+            this.buttonArmor.Size = new System.Drawing.Size(157, 108);
             this.buttonArmor.TabIndex = 2;
-            this.buttonArmor.Text = "&Rüstungen";
-            this.buttonArmor.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.buttonArmor.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.toolTip1.SetToolTip(this.buttonArmor, "Rüstungen");
             this.buttonArmor.UseVisualStyleBackColor = true;
             this.buttonArmor.Click += new System.EventHandler(this.buttonArmor_Click);
             // 
             // buttonQuit
             // 
             this.buttonQuit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.buttonQuit.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonQuit.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.buttonQuit.Image = global::Tesserakt.Properties.Resources.cross_circle;
-            this.buttonQuit.Location = new System.Drawing.Point(0, 204);
+            this.buttonQuit.Location = new System.Drawing.Point(0, 228);
             this.buttonQuit.Name = "buttonQuit";
-            this.buttonQuit.Size = new System.Drawing.Size(234, 34);
+            this.buttonQuit.Size = new System.Drawing.Size(485, 34);
             this.buttonQuit.TabIndex = 6;
             this.buttonQuit.Text = "&Beenden";
             this.buttonQuit.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -83,58 +86,80 @@
             // 
             // buttonTraits
             // 
-            this.buttonTraits.Dock = System.Windows.Forms.DockStyle.Top;
-            this.buttonTraits.Image = ((System.Drawing.Image)(resources.GetObject("buttonTraits.Image")));
-            this.buttonTraits.Location = new System.Drawing.Point(0, 136);
+            this.buttonTraits.AutoSize = true;
+            this.buttonTraits.BackgroundImage = global::Tesserakt.Properties.Resources.traits;
+            this.buttonTraits.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonTraits.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonTraits.Location = new System.Drawing.Point(164, 117);
             this.buttonTraits.Name = "buttonTraits";
-            this.buttonTraits.Size = new System.Drawing.Size(234, 34);
+            this.buttonTraits.Size = new System.Drawing.Size(155, 108);
             this.buttonTraits.TabIndex = 4;
-            this.buttonTraits.Text = "&Eigenschaften";
-            this.buttonTraits.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.buttonTraits.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.toolTip1.SetToolTip(this.buttonTraits, "Eigenschaften");
             this.buttonTraits.UseVisualStyleBackColor = true;
             this.buttonTraits.Click += new System.EventHandler(this.buttonTraits_Click);
             // 
             // buttonEquipment
             // 
-            this.buttonEquipment.Dock = System.Windows.Forms.DockStyle.Top;
-            this.buttonEquipment.Image = ((System.Drawing.Image)(resources.GetObject("buttonEquipment.Image")));
-            this.buttonEquipment.Location = new System.Drawing.Point(0, 102);
+            this.buttonEquipment.AutoSize = true;
+            this.buttonEquipment.BackgroundImage = global::Tesserakt.Properties.Resources.equipment;
+            this.buttonEquipment.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonEquipment.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonEquipment.Location = new System.Drawing.Point(3, 117);
             this.buttonEquipment.Name = "buttonEquipment";
-            this.buttonEquipment.Size = new System.Drawing.Size(234, 34);
+            this.buttonEquipment.Size = new System.Drawing.Size(155, 108);
             this.buttonEquipment.TabIndex = 3;
-            this.buttonEquipment.Text = "Ausrüstung";
-            this.buttonEquipment.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.buttonEquipment.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.toolTip1.SetToolTip(this.buttonEquipment, "Ausrüstung");
             this.buttonEquipment.UseVisualStyleBackColor = true;
             this.buttonEquipment.Click += new System.EventHandler(this.buttonEquipment_Click);
             // 
             // buttonFactions
             // 
-            this.buttonFactions.Dock = System.Windows.Forms.DockStyle.Top;
-            this.buttonFactions.Image = ((System.Drawing.Image)(resources.GetObject("buttonFactions.Image")));
-            this.buttonFactions.Location = new System.Drawing.Point(0, 170);
+            this.buttonFactions.AutoSize = true;
+            this.buttonFactions.BackgroundImage = global::Tesserakt.Properties.Resources.factions;
+            this.buttonFactions.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonFactions.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonFactions.Location = new System.Drawing.Point(325, 117);
             this.buttonFactions.Name = "buttonFactions";
-            this.buttonFactions.Size = new System.Drawing.Size(234, 34);
+            this.buttonFactions.Size = new System.Drawing.Size(157, 108);
             this.buttonFactions.TabIndex = 5;
-            this.buttonFactions.Text = "&Fraktionen";
-            this.buttonFactions.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.buttonFactions.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.toolTip1.SetToolTip(this.buttonFactions, "Fraktionen");
             this.buttonFactions.UseVisualStyleBackColor = true;
             this.buttonFactions.Click += new System.EventHandler(this.buttonFactions_Click);
             // 
             // buttonActors
             // 
-            this.buttonActors.Dock = System.Windows.Forms.DockStyle.Top;
-            this.buttonActors.Image = global::Tesserakt.Properties.Resources.application_list;
-            this.buttonActors.Location = new System.Drawing.Point(0, 0);
+            this.buttonActors.AutoSize = true;
+            this.buttonActors.BackgroundImage = global::Tesserakt.Properties.Resources.models;
+            this.buttonActors.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonActors.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonActors.Location = new System.Drawing.Point(3, 3);
             this.buttonActors.Name = "buttonActors";
-            this.buttonActors.Size = new System.Drawing.Size(234, 34);
+            this.buttonActors.Size = new System.Drawing.Size(155, 108);
             this.buttonActors.TabIndex = 0;
-            this.buttonActors.Text = "&Modelle";
-            this.buttonActors.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.buttonActors.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.toolTip1.SetToolTip(this.buttonActors, "Modelle");
             this.buttonActors.Click += new System.EventHandler(this.buttonActors_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Controls.Add(this.buttonActors, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonWeapons, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonFactions, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.buttonArmor, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonTraits, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.buttonEquipment, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(485, 228);
+            this.tableLayoutPanel1.TabIndex = 7;
             // 
             // MasterDataMainForm
             // 
@@ -142,14 +167,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.CancelButton = this.buttonQuit;
-            this.ClientSize = new System.Drawing.Size(234, 224);
+            this.ClientSize = new System.Drawing.Size(485, 262);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.buttonQuit);
-            this.Controls.Add(this.buttonFactions);
-            this.Controls.Add(this.buttonTraits);
-            this.Controls.Add(this.buttonEquipment);
-            this.Controls.Add(this.buttonArmor);
-            this.Controls.Add(this.buttonWeapons);
-            this.Controls.Add(this.buttonActors);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -157,7 +177,10 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Tesserakt Stammdaten";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MasterDataMainForm_FormClosing);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -170,5 +193,7 @@
         private System.Windows.Forms.Button buttonEquipment;
         private System.Windows.Forms.Button buttonFactions;
         private System.Windows.Forms.Button buttonActors;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
