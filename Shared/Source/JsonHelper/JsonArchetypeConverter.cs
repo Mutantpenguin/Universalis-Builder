76b15a50f8715a5689133d540fc7234915e7adad
Markus Lobedann <markus.lobedann@gmail.com>
2020-10-21 13:17:33 +0200

    archetypes don't get saved into the actor now, just linked via ID

---
+++ Shared/Source/JsonHelper/JsonArchetypeConverter.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Universalis
+{
+    class JsonArchetypeConverter : JsonConverter
+    {
+        public override bool CanConvert( Type objectType )
+        {
+            return ( objectType == typeof( Archetype ) );
+        }
+
+        public override object ReadJson( JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer )
+        {
+            if( reader == null )
+            {
+                throw new ArgumentNullException( nameof( reader ) );
+            }
+
+            if( null != reader.Value )
+            {
+                return ( MasterDataStorage.Archetype.Get( new Guid( (string)reader.Value ) ) );
+            }
+            else
+            {
+                return ( null );
+            }
+        }
+
+        public override void WriteJson( JsonWriter writer, object value, JsonSerializer serializer )
+        {
+            if( writer == null )
+            {
+                throw new ArgumentNullException( nameof( writer ) );
+            }
+
+            if( null != value )
+            {
+                writer.WriteValue( ( (Archetype)value ).ID );
+            }
+        }
+    }
+}
