e4d9052ce5c48479b992fb6ca949218be3d90617
Markus Lobedann <markus.lobedann@gmail.com>
2021-04-13 23:59:27 +0200

    it's possible to clone universes now

---
+++ Shared/Source/Forms/RepoUrlForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Universalis
+{
+    public partial class RepoUrlForm : Form
+    {
+        public Uri RepositoryURL
+        {
+            get;
+            private set;
+        }
+
+        public RepoUrlForm()
+        {
+            InitializeComponent();
+
+            this.Icon = Shared.Properties.Resources.icon;
+        }
+
+        private void buttonOk_Click( object sender, EventArgs e )
+        {
+            try
+            {
+                RepositoryURL = new Uri( textBoxURL.Text );
+
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
+            catch( UriFormatException ex )
+            {
+                MessageBox.Show( ex.Message );
+            }
+        }
+    }
+}
