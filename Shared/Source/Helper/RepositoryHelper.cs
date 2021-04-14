815cd7df34ae9523f73da16f2c37808f9270e4ad
Markus Lobedann <markus.lobedann@gmail.com>
2021-04-14 16:54:49 +0200

    refactor repository deletion into own function

---
+++ Shared/Source/Helper/RepositoryHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+
+namespace Universalis
+{
+    public class RepositoryHelper
+    {
+        public static void Delete( string path )
+        {
+            // can't delete a git repo just like that since some files are protected so we need to reset their file attributes first
+            var directory = new DirectoryInfo( path ) { Attributes = FileAttributes.Normal };
+
+            foreach( var info in directory.GetFileSystemInfos( "*", SearchOption.AllDirectories ) )
+            {
+                info.Attributes = FileAttributes.Normal;
+            }
+
+            directory.Delete( true );
+        }
+    }
+}
