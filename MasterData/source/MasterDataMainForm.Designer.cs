d9d06749b94c65d3c9b80b4d0b2cb1ccb7ea76ec
Markus Lobedann <markus.lobedann@gmail.com>
2021-06-04 23:49:34 +0200

    icon for DamageEffect

--- MasterData/source/MasterDataMainForm.Designer.cs
+++ MasterData/source/MasterDataMainForm.Designer.cs
@@ -30,16 +30,16 @@
         {
             this.components = new System.ComponentModel.Container();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.buttonQuit = new System.Windows.Forms.Button();
             this.buttonFactions = new System.Windows.Forms.Button();
             this.buttonTraits = new System.Windows.Forms.Button();
             this.buttonArchetypes = new System.Windows.Forms.Button();
             this.buttonWeapons = new System.Windows.Forms.Button();
             this.buttonArmor = new System.Windows.Forms.Button();
             this.buttonEquipment = new System.Windows.Forms.Button();
-            this.buttonActors = new System.Windows.Forms.Button();
-            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
-            this.buttonQuit = new System.Windows.Forms.Button();
             this.buttonDamageEffects = new System.Windows.Forms.Button();
+            this.buttonActors = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -67,6 +67,21 @@
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(636, 537);
             this.tableLayoutPanel1.TabIndex = 7;
+            // 
+            // buttonQuit
+            // 
+            this.buttonQuit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonQuit.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.buttonQuit.Image = global::Universalis.Properties.Resources.cross_circle;
+            this.buttonQuit.Location = new System.Drawing.Point(0, 537);
+            this.buttonQuit.Name = "buttonQuit";
+            this.buttonQuit.Size = new System.Drawing.Size(636, 34);
+            this.buttonQuit.TabIndex = 6;
+            this.buttonQuit.Text = "&Beenden";
+            this.buttonQuit.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.buttonQuit.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.buttonQuit.UseVisualStyleBackColor = true;
+            this.buttonQuit.Click += new System.EventHandler(this.buttonExit_Click);
             // 
             // buttonFactions
             // 
@@ -152,6 +167,20 @@
             this.buttonEquipment.UseVisualStyleBackColor = true;
             this.buttonEquipment.Click += new System.EventHandler(this.buttonEquipment_Click);
             // 
+            // buttonDamageEffects
+            // 
+            this.buttonDamageEffects.AutoSize = true;
+            this.buttonDamageEffects.BackgroundImage = global::Universalis.Properties.Resources.damage_effects;
+            this.buttonDamageEffects.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonDamageEffects.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonDamageEffects.Location = new System.Drawing.Point(321, 405);
+            this.buttonDamageEffects.Name = "buttonDamageEffects";
+            this.buttonDamageEffects.Size = new System.Drawing.Size(312, 129);
+            this.buttonDamageEffects.TabIndex = 9;
+            this.toolTip1.SetToolTip(this.buttonDamageEffects, "Schadenseffekte");
+            this.buttonDamageEffects.UseVisualStyleBackColor = true;
+            this.buttonDamageEffects.Click += new System.EventHandler(this.buttonDamageEffects_Click);
+            // 
             // buttonActors
             // 
             this.buttonActors.AutoSize = true;
@@ -164,35 +193,6 @@
             this.buttonActors.TabIndex = 0;
             this.toolTip1.SetToolTip(this.buttonActors, "Modelle");
             this.buttonActors.Click += new System.EventHandler(this.buttonActors_Click);
-            // 
-            // buttonQuit
-            // 
-            this.buttonQuit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.buttonQuit.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.buttonQuit.Image = global::Universalis.Properties.Resources.cross_circle;
-            this.buttonQuit.Location = new System.Drawing.Point(0, 537);
-            this.buttonQuit.Name = "buttonQuit";
-            this.buttonQuit.Size = new System.Drawing.Size(636, 34);
-            this.buttonQuit.TabIndex = 6;
-            this.buttonQuit.Text = "&Beenden";
-            this.buttonQuit.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.buttonQuit.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.buttonQuit.UseVisualStyleBackColor = true;
-            this.buttonQuit.Click += new System.EventHandler(this.buttonExit_Click);
-            // 
-            // buttonDamageEffects
-            // 
-            this.buttonDamageEffects.AutoSize = true;
-            this.buttonDamageEffects.BackgroundImage = global::Universalis.Properties.Resources.equipment;
-            this.buttonDamageEffects.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.buttonDamageEffects.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.buttonDamageEffects.Location = new System.Drawing.Point(321, 405);
-            this.buttonDamageEffects.Name = "buttonDamageEffects";
-            this.buttonDamageEffects.Size = new System.Drawing.Size(312, 129);
-            this.buttonDamageEffects.TabIndex = 9;
-            this.toolTip1.SetToolTip(this.buttonDamageEffects, "Schadenseffekte");
-            this.buttonDamageEffects.UseVisualStyleBackColor = true;
-            this.buttonDamageEffects.Click += new System.EventHandler(this.buttonDamageEffects_Click);
             // 
             // MasterDataMainForm
             // 
