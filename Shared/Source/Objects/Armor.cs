4c186579b9f6dc40fa473a20041489eac2f9f65c
Markus Lobedann <markus.lobedann@gmail.com>
2021-07-25 01:26:34 +0200

    fixes ToolTips for armor/weapon/equipment

--- Shared/Source/Objects/Armor.cs
+++ Shared/Source/Objects/Armor.cs
@@ -296,5 +296,32 @@ namespace Universalis
 
         [JsonIgnore]
         public string EffectsString => ( DamageEffect.GetEffectListString( DamageEffectList ) );
+
+        public override string ToString()
+        {
+            string text = String.Empty;
+
+            if( null != this.ProfileModifier )
+            {
+                string attributeModifierString = this.ProfileModifier.ToString();
+
+                if( !String.IsNullOrEmpty( attributeModifierString ) )
+                {
+                    text += attributeModifierString;
+                }
+            }
+
+            if( !String.IsNullOrEmpty( this.Rules ) )
+            {
+                if( !String.IsNullOrEmpty( text ) )
+                {
+                    text += Environment.NewLine;
+                }
+
+                text += this.Rules;
+            }
+
+            return ( text );
+        }
     }
 }
