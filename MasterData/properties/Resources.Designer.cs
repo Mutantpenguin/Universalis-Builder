089e2e52054fb6bd7b8c0692f87d6bc3844a7c57
Markus Lobedann <markus.lobedann@gmail.com>
2021-08-17 03:20:04 +0200

    removes obsolete icon #20

--- MasterData/properties/Resources.Designer.cs
+++ MasterData/properties/Resources.Designer.cs
@@ -223,16 +223,6 @@ namespace Universalis.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap refresh {
-            get {
-                object obj = ResourceManager.GetObject("refresh", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap text_snippet {
             get {
                 object obj = ResourceManager.GetObject("text_snippet", resourceCulture);
