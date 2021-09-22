6b7c259c4992d1cd93acb456feb1a1f0d9162f2e
Markus Lobedann <markus.lobedann@gmail.com>
2021-09-22 22:11:48 +0200

    cleans up some obsolete icons

--- Groups/Properties/Resources.Designer.cs
+++ Groups/Properties/Resources.Designer.cs
@@ -203,16 +203,6 @@ namespace Universalis.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap image {
-            get {
-                object obj = ResourceManager.GetObject("image", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap plus {
             get {
                 object obj = ResourceManager.GetObject("plus", resourceCulture);
