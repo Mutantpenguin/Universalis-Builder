2c7052b53ea8c11e49d25da8923587f42400e8eb
Markus Lobedann <markus.lobedann@gmail.com>
2022-04-29 21:41:52 +0200

    updates version

--- GlobalAssemblyInfo/GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo/GlobalAssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany( "" )]
 [assembly: AssemblyProduct( "Universalis Program Suite" )]
 
-[assembly: AssemblyCopyright( "Copyright © Markus Lobedann 2021" )]
+[assembly: AssemblyCopyright( "Copyright © Markus Lobedann 2022" )]
 [assembly: AssemblyTrademark( "" )]
 
 [assembly: AssemblyCulture( "" )]
@@ -20,5 +20,5 @@ using System.Runtime.InteropServices;
     [assembly: AssemblyConfiguration("Release")]
 #endif
 
-[assembly: AssemblyVersion( "0.6.0.0" )]
+[assembly: AssemblyVersion( "0.7.0.0" )]
 [assembly: NeutralResourcesLanguage( "de-DE" )]
