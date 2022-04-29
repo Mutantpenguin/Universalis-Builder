ef4271c65ea77a1aa2f114bf06c0010011c13936
Markus Lobedann <markus.lobedann@gmail.com>
2022-04-29 17:08:03 +0200

    dialog for adding a discipline to a model #72

---
+++ Main/source/UserData/Actor/AddDisciplineToActorForm.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace Universalis
+{
+    partial class AddDisciplineToActorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if( disposing && ( components != null ) )
+            {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripTextBoxSearch = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripButtonClearSearch = new System.Windows.Forms.ToolStripButton();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.dataGridViewDisciplines = new System.Windows.Forms.DataGridView();
+            this.disciplineBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MaxLevel = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PointsString = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.toolStrip1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDisciplines)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.disciplineBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripTextBoxSearch,
+            this.toolStripButtonClearSearch});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(343, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripTextBoxSearch
+            // 
+            this.toolStripTextBoxSearch.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.toolStripTextBoxSearch.Name = "toolStripTextBoxSearch";
+            this.toolStripTextBoxSearch.Size = new System.Drawing.Size(100, 25);
+            this.toolStripTextBoxSearch.KeyDown += new System.Windows.Forms.KeyEventHandler(this.toolStripTextBoxSearch_KeyDown);
+            this.toolStripTextBoxSearch.TextChanged += new System.EventHandler(this.toolStripTextBoxSearch_TextChanged);
+            // 
+            // toolStripButtonClearSearch
+            // 
+            this.toolStripButtonClearSearch.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButtonClearSearch.Image = global::Universalis.Properties.Resources.clear;
+            this.toolStripButtonClearSearch.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.toolStripButtonClearSearch.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonClearSearch.Name = "toolStripButtonClearSearch";
+            this.toolStripButtonClearSearch.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButtonClearSearch.ToolTipText = "Text löschen";
+            this.toolStripButtonClearSearch.Click += new System.EventHandler(this.toolStripButtonClearSearch_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.AutoSize = true;
+            this.panel2.Controls.Add(this.buttonCancel);
+            this.panel2.Controls.Add(this.buttonOk);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 394);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(343, 32);
+            this.panel2.TabIndex = 3;
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCancel.Image = global::Universalis.Properties.Resources.cross_circle;
+            this.buttonCancel.Location = new System.Drawing.Point(3, 3);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(100, 26);
+            this.buttonCancel.TabIndex = 3;
+            this.buttonCancel.Text = "Abbrechen";
+            this.buttonCancel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.buttonCancel.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            // 
+            // buttonOk
+            // 
+            this.buttonOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.buttonOk.Image = global::Universalis.Properties.Resources.tick;
+            this.buttonOk.Location = new System.Drawing.Point(240, 3);
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.Size = new System.Drawing.Size(100, 26);
+            this.buttonOk.TabIndex = 2;
+            this.buttonOk.Text = "Übernehmen";
+            this.buttonOk.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.buttonOk.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.buttonOk.UseVisualStyleBackColor = true;
+            this.buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
+            // 
+            // dataGridViewDisciplines
+            // 
+            this.dataGridViewDisciplines.AllowUserToAddRows = false;
+            this.dataGridViewDisciplines.AllowUserToDeleteRows = false;
+            this.dataGridViewDisciplines.AllowUserToOrderColumns = true;
+            this.dataGridViewDisciplines.AllowUserToResizeRows = false;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.dataGridViewDisciplines.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridViewDisciplines.AutoGenerateColumns = false;
+            this.dataGridViewDisciplines.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewDisciplines.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nameDataGridViewTextBoxColumn,
+            this.MaxLevel,
+            this.PointsString});
+            this.dataGridViewDisciplines.DataSource = this.disciplineBindingSource;
+            this.dataGridViewDisciplines.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewDisciplines.Location = new System.Drawing.Point(0, 25);
+            this.dataGridViewDisciplines.Name = "dataGridViewDisciplines";
+            this.dataGridViewDisciplines.ReadOnly = true;
+            this.dataGridViewDisciplines.RowHeadersVisible = false;
+            this.dataGridViewDisciplines.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridViewDisciplines.Size = new System.Drawing.Size(343, 369);
+            this.dataGridViewDisciplines.TabIndex = 4;
+            this.dataGridViewDisciplines.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewDisciplines_CellDoubleClick);
+            this.dataGridViewDisciplines.CellFormatting += new System.Windows.Forms.DataGridViewCellFormattingEventHandler(this.dataGridViewDisciplines_CellFormatting);
+            this.dataGridViewDisciplines.CellToolTipTextNeeded += new System.Windows.Forms.DataGridViewCellToolTipTextNeededEventHandler(this.dataGridViewDisciplines_CellToolTipTextNeeded);
+            this.dataGridViewDisciplines.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dataGridViewDisciplines_KeyDown);
+            // 
+            // disciplineBindingSource
+            // 
+            this.disciplineBindingSource.DataSource = typeof(Universalis.Discipline);
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            this.nameDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // MaxLevel
+            // 
+            this.MaxLevel.DataPropertyName = "MaxLevel";
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.MaxLevel.DefaultCellStyle = dataGridViewCellStyle2;
+            this.MaxLevel.HeaderText = "Max";
+            this.MaxLevel.Name = "MaxLevel";
+            this.MaxLevel.ReadOnly = true;
+            this.MaxLevel.Width = 35;
+            // 
+            // PointsString
+            // 
+            this.PointsString.DataPropertyName = "PointsString";
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.PointsString.DefaultCellStyle = dataGridViewCellStyle3;
+            this.PointsString.HeaderText = "Punkte";
+            this.PointsString.Name = "PointsString";
+            this.PointsString.ReadOnly = true;
+            this.PointsString.Width = 80;
+            // 
+            // AddDisciplineToActorForm
+            // 
+            this.AcceptButton = this.buttonOk;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.buttonCancel;
+            this.ClientSize = new System.Drawing.Size(343, 426);
+            this.ControlBox = false;
+            this.Controls.Add(this.dataGridViewDisciplines);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "AddDisciplineToActorForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Disziplinenauswahl";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDisciplines)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.disciplineBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Button buttonOk;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBoxSearch;
+        private System.Windows.Forms.DataGridView dataGridViewDisciplines;
+        private System.Windows.Forms.BindingSource disciplineBindingSource;
+        private System.Windows.Forms.ToolStripButton toolStripButtonClearSearch;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MaxLevel;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PointsString;
+    }
+}
---
+++ Main/source/UserData/Actor/AddDisciplineToActorForm.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Universalis
+{
+    public partial class AddDisciplineToActorForm : Form
+    {
+        public AddDisciplineToActorForm( Archetype archetype, List<Discipline> disciplinesList )
+        {
+            InitializeComponent();
+
+            m_archetype = archetype;
+            m_disciplineList = disciplinesList;
+
+            updateDataGridViewDisciplines();
+
+            toolStripTextBoxSearch.TextBox.Select();
+        }
+
+        private readonly Archetype m_archetype;
+        private readonly List<Discipline> m_disciplineList;
+
+        public List<Discipline> SelectedDisciplines
+        {
+            get;
+            private set;
+        }
+
+        private void updateDataGridViewDisciplines()
+        {
+            disciplineBindingSource.DataSource = MasterDataStorage.Discipline.Disciplines.Where( s => s.Active )
+                                                                          .Where( s => !m_disciplineList.Any( x => x.ID == s.ID ) )
+                                                                          .Where( s => s.Name.ToUpper().Contains( toolStripTextBoxSearch.Text.ToUpper() ) )
+                                                                          .Where( s => s.Permissions?.Granted( m_archetype ) ?? true )
+                                                                          .OrderBy( x => x.Name )
+                                                                          .ToList();
+        }
+
+        private void buttonOk_Click( object sender, EventArgs e )
+        {
+            CloseWithSelected();
+        }
+
+        private void CloseWithSelected()
+        {
+            SelectedDisciplines = new List<Discipline>();
+
+            for( int i = 0; i < dataGridViewDisciplines.SelectedRows.Count; i++ )
+            {
+                SelectedDisciplines.Add( (Discipline)dataGridViewDisciplines.Rows[ dataGridViewDisciplines.SelectedRows[ i ].Index ].DataBoundItem );
+            }
+
+            Close();
+        }
+
+        private void toolStripTextBoxSearch_TextChanged( object sender, EventArgs e )
+        {
+            updateDataGridViewDisciplines();
+        }
+
+        private void toolStripMenuItemPositives_CheckedChanged( object sender, EventArgs e )
+        {
+            updateDataGridViewDisciplines();
+        }
+
+        private void toolStripMenuItemNegatives_CheckedChanged( object sender, EventArgs e )
+        {
+            updateDataGridViewDisciplines();
+        }
+
+        private void toolStripMenuItemNeutrals_CheckedChanged( object sender, EventArgs e )
+        {
+            updateDataGridViewDisciplines();
+        }
+
+        private void dataGridViewDisciplines_CellDoubleClick( object sender, DataGridViewCellEventArgs e )
+        {
+            if( -1 != e.RowIndex )
+            {
+                SelectedDisciplines = new List<Discipline>
+                {
+                    (Discipline)dataGridViewDisciplines.Rows[ e.RowIndex ].DataBoundItem
+                };
+
+                this.DialogResult = DialogResult.OK;
+                Close();
+            }
+        }
+
+        private void dataGridViewDisciplines_CellToolTipTextNeeded( object sender, DataGridViewCellToolTipTextNeededEventArgs e )
+        {
+            if( e.RowIndex > -1 )
+            {
+                Discipline discipline = (Discipline)dataGridViewDisciplines.Rows[ e.RowIndex ].DataBoundItem;
+
+                e.ToolTipText = discipline.Name + ":" + Environment.NewLine + ToolTipHelper.FormatMaxWidth( discipline.Description );
+            }
+        }
+
+        private void toolStripButtonClearSearch_Click( object sender, EventArgs e )
+        {
+            toolStripTextBoxSearch.Clear();
+        }
+
+        private void dataGridViewDisciplines_KeyDown( object sender, KeyEventArgs e )
+        {
+            if( e.KeyCode == Keys.Return )
+            {
+                this.DialogResult = DialogResult.OK;
+                e.Handled = true;
+                CloseWithSelected();
+            }
+        }
+
+        private void toolStripTextBoxSearch_KeyDown( object sender, KeyEventArgs e )
+        {
+            if( DataGridViewHelper.HandleArrowUpDown( dataGridViewDisciplines, e.KeyCode ) )
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void dataGridViewDisciplines_CellFormatting( object sender, DataGridViewCellFormattingEventArgs e )
+        {
+            DataGridViewHelper.MemberPropertyFormatter( e, dataGridViewDisciplines );
+        }
+    }
+}
