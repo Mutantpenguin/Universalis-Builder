f2bc9a1e508178814428849d002c2266bb599b15
Markus Lobedann <markus.lobedann@gmail.com>
2023-12-25 21:14:09 +0100

    correctly anchor label

--- Main/source/MasterData/Armor/ArmorEditorForm.Designer.cs
+++ Main/source/MasterData/Armor/ArmorEditorForm.Designer.cs
@@ -188,7 +188,6 @@
             // 
             // label3
             // 
-            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(3, 82);
             this.label3.Name = "label3";
