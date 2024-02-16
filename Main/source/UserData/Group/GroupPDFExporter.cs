3470d31206a4a944367bc38c5a0d6e136590b3e1
Markus Lobedann <markus.lobedann@gmail.com>
2024-02-16 23:08:47 +0100

    no activation cards for Telematons

--- Main/source/UserData/Group/GroupPDFExporter.cs
+++ Main/source/UserData/Group/GroupPDFExporter.cs
@@ -460,7 +460,7 @@ namespace Universalis
                 }
             }
 
-            List<Actor> sortedActorList = group.Models.Where( x => x.Active ).ToList();
+            List<Actor> sortedActorList = group.Models.Where( x => x.Active ).Where( x => x.Archetype.Type != Archetype.EType.Telematon ).ToList();
 
             for( int i = 0; i < sortedActorList.Count; i++ )
             {
