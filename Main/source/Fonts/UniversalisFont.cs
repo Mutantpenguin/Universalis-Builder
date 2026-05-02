8a3d4907863b7ac8417b2c7aea9c9f07100d69ae
Markus Lobedann <markus.lobedann@gmail.com>
2026-05-02 22:55:10 +0200

    don't dispose of PFC as this can lead to crashes or fallback to a system font

--- Main/source/Fonts/UniversalisFont.cs
+++ Main/source/Fonts/UniversalisFont.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 using System.Drawing;
 using System.Drawing.Text;
 using System.Linq;
@@ -12,19 +11,27 @@ namespace Universalis
         [DllImport("gdi32.dll")]
         private static extern IntPtr AddFontMemResourceEx(IntPtr pbFont, uint cbFont, IntPtr pdv, in uint pcFonts);
 
+        private static readonly PrivateFontCollection PFC;
+
         static UniversalisFont()
         {
-            using( PrivateFontCollection pfc = new PrivateFontCollection() )
-            {
-                // load Font from Resource
-                byte[] fontData = Properties.Resources.NovaFlat_Regular;
-                IntPtr fontPtr = Marshal.AllocCoTaskMem( fontData.Length );
-                Marshal.Copy( fontData, 0, fontPtr, fontData.Length );
-                AddFontMemResourceEx(fontPtr, (uint)fontData.Length, IntPtr.Zero, 0);
-                pfc.AddMemoryFont( fontPtr, fontData.Length );
-                Marshal.FreeCoTaskMem( fontPtr );
+            PFC = new PrivateFontCollection();
 
-                Family = pfc.Families.First( s => s.Name.Equals( Name ) );
+            // load Font from Resource
+            byte[] fontData = Properties.Resources.NovaFlat_Regular;
+            IntPtr fontPtr = Marshal.AllocCoTaskMem(fontData.Length);
+            try
+            {
+                Marshal.Copy(fontData, 0, fontPtr, fontData.Length);
+
+                AddFontMemResourceEx(fontPtr, (uint)fontData.Length, IntPtr.Zero, 0);
+                PFC.AddMemoryFont(fontPtr, fontData.Length);
+
+                Family = PFC.Families.First(s => s.Name.Equals(Name));
+            }
+            finally
+            {
+                Marshal.FreeCoTaskMem(fontPtr);
             }
         }
 
